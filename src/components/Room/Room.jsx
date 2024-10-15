946e8018031cd3504db8ece83db960c59f3f4f13
Rasmus <rasmu@LAPTOP-08K6AG11>
2024-10-15 11:48:39 +0200

    Installed sockets :D

--- src/components/Room/Room.jsx
+++ src/components/Room/Room.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { React } from 'react'
 import { useParams } from 'react-router-dom'
+import socket from '../../socket'
 
 const Room = (props) => {
 	const { roomId } = useParams();
+
+    navigator.mediaDevices
+        .getUserMedia({ audio: true })
+        .then(stream => {
+            
+        })
+
+
 	return (<p>Hello World from Room: {roomId}</p>)
 }
 
