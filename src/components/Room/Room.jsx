c634a2cfbf7ad4f59efe5557160b26aa9308f58d
runeb <runeberhardt@gmail.com>
2024-10-17 09:31:00 +0200

    enter-room with room_uuid

--- src/components/Room/Room.jsx
+++ src/components/Room/Room.jsx
@@ -1,47 +1,54 @@
-import { React } from 'react'
-import { useParams } from 'react-router-dom'
-import socket from '../../socket'
+	import { React, useEffect } from 'react'
+	import { useParams } from 'react-router-dom'
+	import socket from '../../socket'
 
-const Room = (props) => {
-	var username
-	const { roomId } = useParams();
-	return (
-	<div>
-		<p>Hello World from Room: {roomId}</p>
-		<p>Type username below:</p>
-		<input type='text' id="usernameInputField" value={username} placeholder='Johnny' required></input>
-		<button onClick={createUsername}>Confirm Username</button>
-		<br></br>
-		<button onClick={logSessionData}>Console log session data</button>
-	</div>
-	
+	const Room = (props) => {
+		var username
+		const { roomId } = useParams();
 
-)
-}
+		useEffect(() => {
+			socket.emit("BE-enter-room", "uuid")
+			console.log("Emitted BE-enter-room event with roomId:", roomId)
+		}, [],)
+		
+		return (
+		<div>
+			<p>Hello World from Room: {roomId}</p>
+			<p>Type username below:</p>
+			<input type='text' id="usernameInputField" value={username} placeholder='Johnny' required></input>
+			<button onClick={createUsername}>Confirm Username</button>
+			<br></br>
+			<button onClick={logSessionData}>Console log session data</button>
+			<button onClick={logSessionData}></button>
+		</div>
+		
 
-// TODO: Lav input handling!!! det her extremt vulnerable lol. 
-// TODO: Brug hooks til at håndtere state på inputfeltet. useState hook er smart her.
-async function createUsername () {
-	const username = document.getElementById('usernameInputField').value
-	console.log("sendding socket to backend. username = " + username)
-	// TODO: BE er en hemmelighed
-	socket.emit("BE-create-username", {
-		username
+	)
+	}
+
+	// TODO: Lav input handling!!! det her extremt vulnerable lol. 
+	// TODO: Brug hooks til at håndtere state på inputfeltet. useState hook er smart her.
+	async function createUsername () {
+		const username = document.getElementById('usernameInputField').value
+		console.log("sendding socket to backend. username = " + username)
+		// TODO: BE er en hemmelighed
+		socket.emit("BE-create-username", {
+			username
+		})
+		
+	}
+
+	function requestSessionData() {
+		socket.emit("BE-get-session");
+	}
+
+	socket.on("FE-session-data", (sessionData) => {
+		console.log("Session data received:", sessionData);
 	})
-	
-}
 
-function requestSessionData() {
-    socket.emit("BE-get-session");
-}
-
-socket.on("FE-session-data", (sessionData) => {
-    console.log("Session data received:", sessionData);
-})
-
-function logSessionData() {
-	requestSessionData()
-}
+	function logSessionData() {
+		requestSessionData()
+	}
 
 
-export default Room
+	export default Room
