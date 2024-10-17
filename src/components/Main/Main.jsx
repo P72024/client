12d17818815c73aa497d14d6f354ad5b44cc3758
runeb <runeberhardt@gmail.com>
2024-10-17 15:23:31 +0200

    join existing room functionality

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,13 +5,8 @@ const Main = () => {
 	let username
 	const navigate = useNavigate()
 
-	async function createRoom() {
-		const response = await fetch('http://127.0.0.1:8080/createRoomUUID', {
-			method: 'GET',
-		})
-		const uuid = await response.text()
-
-        // TODO: Lav input handling!!! det her extremt vulnerable lol. 
+	async function sendUsername() {
+		// TODO: Lav input handling!!! det her extremt vulnerable lol. 
         // TODO: Brug hooks til at håndtere state på inputfeltet. useState hook er smart her.
         const username = document.getElementById("usernameInputField").value
         console.log("sending socket to backend. username = " + username)
@@ -19,6 +14,17 @@ const Main = () => {
         await socket.emit("BE-create-username", {
             username
         })
+	}
+
+	async function createRoom() {
+		await sendUsername()
+
+		const response = await fetch('http://127.0.0.1:8080/createRoomUUID', {
+			method: 'GET',
+		})
+		const uuid = await response.text()
+
+        
 		console.log("uuid: " + uuid)
 
 		await socket.emit("BE-enter-room", {
@@ -33,11 +39,28 @@ const Main = () => {
 		
 		}
 
+	async function joinRoom() {
+		await sendUsername()
+		socket.emit("BE-send-room-uuid", (uuid) => {
+			socket.emit("BE-enter-room", {
+				uuid
+			}, (response) => {
+				if (response === "OK") {
+					window.history.pushState({}, `Room: ${uuid}`, `/room/${uuid}`)
+					navigate(`/room/${uuid}`)
+				}
+				else console.log("No room created. Response: " + response)
+			})
+		})
+	}
+
 	return (<div>
 		<h1>Welcome to the most awesome Conference App </h1>
 		<h2>Please choose a Username and create a new room :)</h2>
         <input type='text' id="usernameInputField" value={username} placeholder='Johnny' required></input>
 		<button onClick={createRoom}>Create Room</button>
+		<br></br>
+		<button onClick={joinRoom}>Join Room</button>
 	</div>
 	)
 }
